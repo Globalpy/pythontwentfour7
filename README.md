5bcfd4ca2e957d521a7f2db14afb1bebb3a98221
Globalpy <pythonglobal247@gmail.com>
2024-03-01 08:30:11 +0200

    Our first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# pythontwentfour7" 
+This is meant to build our python and git skills
---
+++ calculator.py
@@ -0,0 +1,6 @@
+#this is a calculator program
+def add(num1,num2):
+    return num1+num2
+
+print(add(1,9))
+#add more functions here
